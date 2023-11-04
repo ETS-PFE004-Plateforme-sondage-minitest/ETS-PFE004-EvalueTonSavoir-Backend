674f4ccc1df62319a03ca8570afab9b52d11b52e
Paul <berguin.paul@gmail.com>
2023-11-04 16:18:00 -0400

    prettier + id in submit-answer

--- socket/socket.js
+++ socket/socket.js
@@ -1,71 +1,72 @@
-const socketIo = require('socket.io');
+const socketIo = require("socket.io");
 
 const setupWebsocket = (server) => {
-    const io = socketIo(server, {
-        path: '/socket.io',
-        cors: {
-            origin: '*',
-            methods: ["GET", "POST"],
-            credentials: true
-        }
+  const io = socketIo(server, {
+    path: "/socket.io",
+    cors: {
+      origin: "*",
+      methods: ["GET", "POST"],
+      credentials: true,
+    },
+  });
+
+  io.on("connection", (socket) => {
+    console.log("A user connected:", socket.id);
+
+    socket.on("create-room", () => {
+      const roomName = generateRoomName();
+      if (!io.sockets.adapter.rooms.get(roomName)) {
+        socket.join(roomName);
+        socket.emit("create-success", roomName);
+      } else {
+        socket.emit("create-failure"); //TODO - handle (backend and frontend) create-failure
+      }
     });
 
-    io.on('connection', (socket) => {
-        console.log('A user connected:', socket.id);
-
-        socket.on('create-room', () => {
-            const  roomName = generateRoomName();
-            if(!io.sockets.adapter.rooms.get(roomName)){
-                socket.join(roomName);
-                socket.emit('create-success', roomName);
-            }
-            else{
-                socket.emit('create-failure'); //TODO - handle (backend and frontend) create-failure
-            }
-        });
-
-
-        socket.on('join-room', ({enteredRoomName, username}) => {
-            if (io.sockets.adapter.rooms.has(enteredRoomName)) {
-                socket.join(enteredRoomName);
-                console.log(`User ${socket.id} joined room: ${enteredRoomName}`);
-                socket.to(enteredRoomName).emit('user-joined', username);
-                socket.emit('join-success');
-            } else {
-                socket.emit('join-failure');
-            }
-        });
-
-        socket.on('next-question', ({roomName, question}) => {
-            socket.to(roomName).emit('next-question', question);
-        });
-
-        socket.on('end-quiz',({roomName}) => {
-            socket.to(roomName).emit('end-quiz');
-        });
-
-        socket.on('message', (data) => {
-            console.log('Received message from', socket.id, ':', data);
-        });
-
-        socket.on('disconnect', () => {
-            console.log('A user disconnected:', socket.id);
-        });
-
-        socket.on("submit-answer", ({roomName, username, answer}) => {
-            socket.to(roomName).emit('submit-answer', {username, answer});
-        });
+    socket.on("join-room", ({ enteredRoomName, username }) => {
+      if (io.sockets.adapter.rooms.has(enteredRoomName)) {
+        socket.join(enteredRoomName);
+        console.log(`User ${socket.id} joined room: ${enteredRoomName}`);
+        socket.to(enteredRoomName).emit("user-joined", username);
+        socket.emit("join-success");
+      } else {
+        socket.emit("join-failure");
+      }
     });
 
+    socket.on("next-question", ({ roomName, question }) => {
+      socket.to(roomName).emit("next-question", question);
+    });
 
-    const generateRoomName = (length = 6) => {
-        const characters = 'ABCDEFGHIJKLMNPQRSTUVWXYZ0123456789';
-        let result = '';
-        for (let i = 0; i < length; i++) {
-            result += characters.charAt(Math.floor(Math.random() * characters.length));
-        }
-        return result;
-    };
-}
+    socket.on("end-quiz", ({ roomName }) => {
+      socket.to(roomName).emit("end-quiz");
+    });
+
+    socket.on("message", (data) => {
+      console.log("Received message from", socket.id, ":", data);
+    });
+
+    socket.on("disconnect", () => {
+      console.log("A user disconnected:", socket.id);
+    });
+
+    socket.on("submit-answer", ({ roomName, username, answer, idQuestion }) => {
+      socket
+        .to(roomName)
+        .emit("submit-answer", { username, answer, idQuestion });
+    });
+  });
+
+  const generateRoomName = (length = 6) => {
+    const characters = "ABCDEFGHIJKLMNPQRSTUVWXYZ0123456789";
+    let result = "";
+    for (let i = 0; i < length; i++) {
+      result += characters.charAt(
+        Math.floor(Math.random() * characters.length)
+      );
+    }
+    return result;
+  };
+};
 
 module.exports = { setupWebsocket };
