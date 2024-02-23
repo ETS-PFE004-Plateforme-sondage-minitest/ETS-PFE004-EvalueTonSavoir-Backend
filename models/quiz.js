38c58f5ae226a0bf26df36180bae9944d71d2a47
Louis-Antoine Caron <louis-antoine.caron.1@ens.etsmtl.ca>
2024-02-23 15:34:09 -0500

    update to quiz API

---
+++ models/quiz.js
@@ -0,0 +1,80 @@
+const db = require('../config/db.js')
+const { ObjectId } = require('mongodb');
+
+class Quiz {
+    
+    async create(title, content, folderId, userId) {
+        await db.connect()
+        const conn = db.getConnection();
+
+        const quizCollection = conn.collection('files');
+
+        const existingQuiz = await quizCollection.findOne({ title: title, folderId: folderId, userId: userId })
+
+        if (existingQuiz) return null;
+
+        const newQuiz = {
+            folderId: folderId,
+            userId: userId,
+            title: title,
+            content: content,
+            created_at: new Date(),
+            updated_at: new Date()
+        }
+
+        const result = await quizCollection.insertOne(newQuiz);
+
+        return result.insertedId;
+    }
+
+    async getOwner(quizId) {
+        await db.connect()
+        const conn = db.getConnection();
+
+        const quizCollection = conn.collection('files');
+
+        const quiz = await quizCollection.findOne({ _id: new ObjectId(quizId) });
+
+        return quiz.userId;
+    }
+
+    async getContent(quizId) {
+        await db.connect()
+        const conn = db.getConnection();
+
+        const quizCollection = conn.collection('files');
+
+        const quiz = await quizCollection.findOne({ _id: new ObjectId(quizId) });
+
+        return quiz;
+    }
+
+    async delete(quizId) {
+        await db.connect()
+        const conn = db.getConnection();
+
+        const quizCollection = conn.collection('files');
+
+        const result = await quizCollection.deleteOne({ _id: new ObjectId(quizId) });
+
+        if (result.deletedCount != 1) return false;
+
+        return true;
+    }
+
+    async update(quizId, newTitle, newContent) {
+        await db.connect()
+        const conn = db.getConnection();
+
+        const quizCollection = conn.collection('files');
+
+        const result = await quizCollection.updateOne({ _id: new ObjectId(quizId) }, { $set: { title: newTitle, content: newContent } });
+
+        if (result.modifiedCount != 1) return false;
+
+        return true
+    }
+    
+}
+
+module.exports = new Quiz;
--- routers/folders.js
+++ routers/folders.js
@@ -8,7 +8,7 @@ router.post("/create", jwt.authenticate, foldersController.create);
 router.get("/getUserFolders", jwt.authenticate, foldersController.getUserFolders);
 router.get("/getFolderContent/:folderId", jwt.authenticate, foldersController.getFolderContent);
 router.delete("/delete/:folderId", jwt.authenticate, foldersController.delete);
-router.post("/rename", jwt.authenticate, foldersController.rename);
+router.put("/rename", jwt.authenticate, foldersController.rename);
 
 router.post("/duplicate", jwt.authenticate, foldersController.duplicate);
 router.post("/copy/:folderId", jwt.authenticate, foldersController.copy);
